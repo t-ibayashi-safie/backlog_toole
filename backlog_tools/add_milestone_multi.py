47cd79cf7baa58a9f1d926dbc157306124e59864
t-ibayashi-safie <t-ibayashi@safie.jp>
2023-02-20 12:45:59 +0900

    特定のチケット種別を無視できるようにする

--- backlog_tools/add_milestone_multi.py
+++ backlog_tools/add_milestone_multi.py
@@ -1,12 +1,19 @@
 import requests
 import json
+import logging
 from backlog_tools.settings import Settings
 
+logger = logging.getLogger()
 
-TARGET_STATUSES = [
-    "処理中",
-]
 MILESTONE_ID = 635640
+
+TARGET_STATUSES = [ # 指定したステータスのIssueが更新対象
+    "処理中",
+    "処理予定",
+]
+IGNORE_ISSUE_TYPES = [ # 指定したタイプのIssueは除外される
+    "PBI",
+]
 
 STATUS_ID_MAP = {}
 PATH_TO_STATUS_MAP = "./data/statuses.json"
@@ -35,23 +42,31 @@ def main():
             "statusId": [STATUS_ID_MAP[status] for status in TARGET_STATUSES],
         }
     )
+    response.raise_for_status()
     issues = json.loads(response.text)
 
     # マイルストーンを更新
     for issue in issues:
         issue_id = issue["id"]
         milestones = issue["milestone"]
+        issue_type = issue["issueType"]["name"]
         milestone_ids = [m["id"] for m in milestones]
+
+        if issue_type in IGNORE_ISSUE_TYPES:
+            continue
+
         if MILESTONE_ID not in milestone_ids:
             milestone_ids.append(MILESTONE_ID)
-
+        print("update ticket! {}".format(issue["summary"]))
         url = f"{base_url}/api/v2/issues/{issue_id}?apiKey={api_key}"
         response = requests.patch(
             url,
             json={
                 "milestoneId": milestone_ids,
+                "comment": "updated_by_script"
             }
         )
+        response.raise_for_status()
     
 
 if __name__ == "__main__":
